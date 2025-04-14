230840b783f0edda501248682339b2c0eef331ca
GokayDemirr <gokaydemirpc@gmail.com>
2025-04-14 23:06:21 +0300

    add header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Header } from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "To-Do App",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen flex justify-center p-8">
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }

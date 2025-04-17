e825277e9cc6925da329d23b9e540664b4491da6
GokayDemirr <gokaydemirpc@gmail.com>
2025-04-17 03:09:29 +0300

    refactor: layout.tsx page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex justify-center p-8">
+      <body className="flex flex-col items-center  p-8">
         <Header />
         <main>{children}</main>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import { AddItem } from "@/components/Todo/AddItem";
+import { TaskList } from "@/components/Todo/TaskList";
+import { Task } from "@/types";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>To-Do-App</div>;
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  return (
+    <div className="flex-col flex items-center gap-8">
+      <AddItem taskList={taskList} setTaskList={setTaskList} />
+      <TaskList taskList={taskList} setTaskList={setTaskList} />
+    </div>
+  );
 }

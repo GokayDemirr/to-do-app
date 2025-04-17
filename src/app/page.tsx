eaa31de39cb9edde435feacb8e02b943df38f478
GokayDemirr <gokaydemirpc@gmail.com>
2025-04-17 03:29:09 +0300

    feat: show how many tasks to do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,14 @@ import { useState } from "react";
 export default function Home() {
   const [taskList, setTaskList] = useState<Task[]>([]);
 
+  const remainingTasks = taskList.filter((t) => !t.completed).length;
+
   return (
     <div className="flex-col flex items-center gap-8">
       <AddItem taskList={taskList} setTaskList={setTaskList} />
+      {remainingTasks
+        ? `You have ${remainingTasks} tasks to do`
+        : "You have nothing to do 😊"}
       <TaskList taskList={taskList} setTaskList={setTaskList} />
     </div>
   );
